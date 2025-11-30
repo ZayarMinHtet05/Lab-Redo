5148baaf27ee158bface71a43ee19402270ade74
Zayar Min Htet <zymhtet05.24@ichat.sp.edu.sg>
2025-11-30 20:54:14 +0800

    Lab Redo

---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height, weight):
+    print("Height =" + str(height) )
+    print("Weight =" + str(weight))
+    bmi = weight/(height*height)
+    print("BMI is " + str(bmi))
+
+    if(bmi<18.5):
+        print("Under Weight")
+    elif(18.5<=bmi<=25.0):
+        print("Normal Weight")
+    elif(bmi>25.0):
+        print("Over Weight")
+        
+calculate_bmi(height=1.73,weight=57)
