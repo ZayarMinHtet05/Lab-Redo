d92139de9ed10bf69fd0f36a70de00785ae90618
Zayar Min Htet <zymhtet05.24@ichat.sp.edu.sg>
2025-12-01 12:30:34 +0800

    Return added

--- bmi.py
+++ bmi.py
@@ -6,9 +6,12 @@ def calculate_bmi(height, weight):
 
     if(bmi<18.5):
         print("Under Weight")
+        return -1
     elif(18.5<=bmi<=25.0):
         print("Normal Weight")
+        return 0
     elif(bmi>25.0):
         print("Over Weight")
+        return 1
         
 calculate_bmi(height=1.73,weight=57)
