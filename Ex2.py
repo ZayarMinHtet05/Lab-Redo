f16b99c0a3e6f2840c67a0ae915b8bf3cc96360f
Zayar Min Htet <zymhtet05.24@ichat.sp.edu.sg>
2025-12-01 12:00:01 +0800

    LAB redo

--- Ex2.py
+++ Ex2.py
@@ -22,6 +22,18 @@ def calc_min_max_temp(num_list):
     Maximum = max(num_list)
     return Minimum,Maximum
 
+def calc_median_temp(num_list):
+    sorted_list = sorted(num_list)
+    length = len(sorted_list)
+    if length %2 == 0:
+        mid1 = length//2-1
+        mid2 = length//2
+        median = (sorted_list[mid1]+sorted_list[mid2])/2
+        return median
+    else: 
+        mid = length//2
+        median = sorted_list[mid] 
+        return median
 
 def main():
     display_main_menu()
@@ -29,6 +41,7 @@ def main():
     print(num_list)
     print("Average : ",calc_average_temp(num_list))
     print("Min & Max : ",calc_min_max_temp(num_list))
+    print("Median : ",calc_median_temp(num_list) )
     
 
 if __name__ == "__main__":
